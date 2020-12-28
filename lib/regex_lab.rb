bc118eead08e479fcc1f69bdc1d4f60890e43073
Alex <alexfrancisaaron@gmail.com>
2020-12-28 09:53:36 -0600

    commit tree ed007fcac263441b4b3df67b48a3bb15ad722a3a

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,34 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+    if word.start_with?(/[aeiouAEIOU]/)
+        true
+    else
+        false 
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+    text.scan(/\b+un\w+ing/)
 
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    if text.scan(/\b[A-Z]\w+[,.!?]/).length > 0
+        true
+    else
+        false
+    end
 end
 
 def valid_phone_number?(phone)
+    if phone.scan(/\b\d+{10}/).length > 0
+        true
+    else
+        false
+    end
 
 end
